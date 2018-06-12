df3aecd628ebd6b56e0c391468089b2ba8d83f71
SOJ <takayashi0825@gmail.com>
2018-06-12 20:18:03 +0800

    update iOS

--- iOS/DrawCards/DrawCards/ViewController.swift
+++ iOS/DrawCards/DrawCards/ViewController.swift
@@ -86,11 +86,11 @@ class ViewController: UIViewController {
             self.secondShiny.image = UIImage.animatedImage(with: secondImages, duration: 2)
             self.self.secondImageView.image = UIImage(named: "character\(secondCharacterRandom)")
             
-            self.firstBackground.addSubview(self.firstShiny)
+//            self.firstBackground.addSubview(self.firstShiny)
             self.firstBackground.addSubview(self.firstImageView)
             self.animationView.addSubview(self.firstBackground)
             
-            self.secondBackground.addSubview(self.secondShiny)
+//            self.secondBackground.addSubview(self.secondShiny)
             self.secondBackground.addSubview(self.secondImageView)
             self.animationView.addSubview(self.secondBackground)
             
@@ -106,6 +106,7 @@ class ViewController: UIViewController {
             
             openButton.setTitle("Open", for: .normal)
             isOpen = true
+            borderImageView.image = UIImage(named: "card_slot_line_94x112-(1)_01")
             shinyTimer = Timer.scheduledTimer(timeInterval:2, target: self , selector: #selector( borderShiny), userInfo: nil , repeats: true )
         }
     }
@@ -120,6 +121,14 @@ class ViewController: UIViewController {
             self.whiteView.alpha = 0
             
         })
+        let firstRandom = Int(arc4random_uniform(4))
+        let firstCharacterRandom = Int(arc4random_uniform(8)+1)
+        let secondRandom = Int(arc4random_uniform(4))
+        let secondCharacterRandom = Int(arc4random_uniform(8)+1)
+        
+        var firstBorderImageName = "card_slot_line_94x112-(1)_0"
+        var secondBorderImageName = "card_slot_line_94x112-(1)_0"
+        
         if timeCount > 0 {
             timeCount -= 1 * timeInterval
 //            print(timeCount)
@@ -127,21 +136,21 @@ class ViewController: UIViewController {
             firstBackground.frame.origin.x -= CGFloat(82 / moveRatio)
             secondBackground.frame.origin.x -= CGFloat(82 / moveRatio)
             if firstBackground.frame.origin.x < -3 {
-                let firstRandom = Int(arc4random_uniform(4))
-                let firstCharacterRandom = Int(arc4random_uniform(8)+1)
+
                 let firstImageName = "card_aura_\(randomColor[firstRandom])_128x128_"
                 let firstImages = createImages(name: firstImageName)
                 firstBackground.image = UIImage(named: "card_slot_60x73_0\(firstRandom+2)")
+                firstBorderImageName += "\(firstRandom+2)"
                 firstShiny.image = UIImage.animatedImage(with: firstImages, duration: 2)
                 firstImageView.image = UIImage(named: "character\(firstCharacterRandom)")
-                firstBackground.frame.origin.x = secondBackground.frame.origin.x + 82
+                firstBackground.frame.origin.x = secondBackground.frame.origin.x + secondBackground.frame.width
             }
             if secondBackground.frame.origin.x < -3 {
-                let secondRandom = Int(arc4random_uniform(4))
-                let secondCharacterRandom = Int(arc4random_uniform(8)+1)
+
                 let secondImageName = "card_aura_\(randomColor[secondRandom])_128x128_"
                 let secondImages = createImages(name: secondImageName)
                 secondBackground.image = UIImage(named: "card_slot_60x73_0\(secondRandom+2)")
+                secondBorderImageName += "\(secondRandom+2)"
                 secondShiny.image = UIImage.animatedImage(with: secondImages, duration: 2)
                 secondImageView.image = UIImage(named: "character\(secondCharacterRandom)")
                 secondBackground.frame.origin.x = firstBackground.frame.origin.x + 82
@@ -154,14 +163,21 @@ class ViewController: UIViewController {
             canReset = true
             UIView.animate(withDuration: 0.5, animations: {
                 self.whiteView.alpha = 0
-                
             })
             if firstBackground.frame.origin.x > 79 && firstBackground.frame.origin.x < 79+41 {
                 firstBackground.frame.origin.x = 79
                 secondBackground.frame.origin.x = 79 + 82
+                borderImageView.image = UIImage(named: firstBorderImageName)
+//                print("first: \(firstRandom)")
+                firstBackground.addSubview(firstShiny)
+                firstImageView.layer.zPosition = 2
             } else {
                 firstBackground.frame.origin.x = 79  + 82
                 secondBackground.frame.origin.x = 79
+                borderImageView.image = UIImage(named: secondBorderImageName)
+//                print("second: \(secondRandom)")
+                secondBackground.addSubview(secondShiny)
+                secondImageView.layer.zPosition = 2
             }
         }
         
@@ -195,7 +211,6 @@ class ViewController: UIViewController {
             
         }
     }
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
