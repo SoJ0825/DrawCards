fd9e271b49b95a8cc5732d3f4b66a3acdbd46eaa
York <kmgs4524@gmail.com>
2018-06-12 16:46:30 +0800

    Commit Android project.

---
+++ Android/DrawCards/app/src/main/java/com/york/android/drawcards/LoadSmoke.kt
@@ -0,0 +1,40 @@
+package com.york.android.drawcards
+
+class LoadSmoke {
+    var smokes = ArrayList<Card>()
+
+    fun loadSmokeBitmap(): ArrayList<Card> {
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_01, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_02, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_03, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_04, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_05, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_06, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_07, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_08, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_09, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_10, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_11, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_12, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_13, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_14, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_15, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_16, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_17, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_18, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_19, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_20, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_21, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_22, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_23, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_24, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_25, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_26, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_27, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_28, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_29, 0))
+        smokes.add(Card(R.drawable.card_aura_gray_128x128_30, 0))
+
+        return smokes
+    }
+}
